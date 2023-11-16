e08f827604fd46d46c448313d73dc9859b644198
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-16 13:33:42 +0300

    Изменил адреса

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('sign-in/', views.SignInView.as_view(), name='sign_in'),
     path('sign-up/', views.SignUpView.as_view(), name='sign_up'),
+    path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile')
 ]
