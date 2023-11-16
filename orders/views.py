d7511e845bab605b5452a1fd2b70de7acc406468
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-16 14:47:14 +0300

    Добавил представление "О нас"

--- orders/views.py
+++ orders/views.py
@@ -7,3 +7,8 @@ from common.mixins import TitleMixin
 class IndexView(TitleMixin, TemplateView):
     template_name = 'orders/index.html'
     title = _('Main')
+
+
+class AboutUsView(TitleMixin, TemplateView):
+    template_name = 'orders/about.html'
+    title = _('About us')
