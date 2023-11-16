76f79836fd4b45c709e3a01f7b6649e04aa7655b
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-16 13:32:28 +0300

    Перенес валидатор номера в common + изменил импорт

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from base.models import Base
-from orders.validators import phone_number_validator
+from common.validators import phone_number_validator
 
 
 class Employee(Base):
