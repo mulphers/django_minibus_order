ab15c846a430f4e905ef06002d2fd9315e4ecaa8
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-16 14:46:23 +0300

    Добавил 2 миксина

--- common/mixins.py
+++ common/mixins.py
@@ -1,3 +1,9 @@
+from django.conf import settings
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+
 class TitleMixin:
     title = None
 
@@ -6,3 +12,22 @@ class TitleMixin:
         context['title'] = self.title
 
         return context
+
+
+class LogoutRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return not self.request.user.is_authenticated
+
+    def handle_no_permission(self):
+        return redirect(reverse_lazy('user:profile', kwargs={'pk': self.request.user.id}))
+
+
+class UserCheckMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.id == self.kwargs.get('pk')
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            return redirect(reverse_lazy('user:profile', kwargs={'pk': self.request.user.id}))
+
+        return redirect(settings.LOGIN_URL)
