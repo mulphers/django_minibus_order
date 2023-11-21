22c98acbdfcb0f656f71971b0dac11f8c58ca602
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-21 12:20:04 +0300

    Добавил путь

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from orders import views
+
 app_name = 'orders'
 
 urlpatterns = [
-
+    path('search/', views.SearchRoutesView.as_view(), name='search'),
+    path('routes/', views.RoutesView.as_view(), name='routes')
 ]
