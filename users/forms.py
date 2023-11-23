0fefdb40c53f4351f21470fae27c78708784b5f9
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-23 18:58:01 +0300

    Добавил ввод номера телефона при регистрации

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth import forms as f
 from django.utils.translation import gettext_lazy as _
 
+from common.validators import phone_number_validator
 from users.models import User
 
 
@@ -46,6 +47,12 @@ class SignUpForm(f.UserCreationForm):
         'placeholder': _('Enter your email address')
     }))
 
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'id': 'inputPhoneNumber',
+        'placeholder': _('Enter your phone number')
+    }), validators=(phone_number_validator,))
+
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4',
         'id': 'inputPassword',
@@ -60,7 +67,7 @@ class SignUpForm(f.UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'username', 'email', 'phone_number', 'password1', 'password2')
 
 
 class ProfileUpdateForm(f.UserChangeForm):
