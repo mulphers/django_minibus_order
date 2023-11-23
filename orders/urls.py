e92cd889666d35fd5e4e2c1233e5414be040f8f8
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-23 17:54:34 +0300

    Добавил пути

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,8 @@ app_name = 'orders'
 
 urlpatterns = [
     path('search/', views.SearchRoutesView.as_view(), name='search'),
-    path('routes/', views.RoutesView.as_view(), name='routes')
+    path('routes/', views.RoutesView.as_view(), name='routes'),
+    path('create-order/<int:route_id>', views.CreateOrderView.as_view(), name='create_order'),
+    path('cancel-order/<int:pk>', views.CancelOrderView.as_view(), name='cancel_order'),
+    path('orders/', views.OrdersView.as_view(), name='orders'),
 ]
