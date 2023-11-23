45f4a84e18d91e0843d7b3f4eb4732fbd5a027b7
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-23 18:58:19 +0300

    Добавил перевод

--- common/validators.py
+++ common/validators.py
@@ -1,8 +1,9 @@
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+
+error_message = _('The phone number must be entered in the format:\n1. +375XXXXXXXXX\n2. 80XXXXXXXXX')
 
 phone_number_validator = RegexValidator(
     regex=r'^((\+375|80)\d{9,9})$',
-    message='Номер телефона должен быть введен в формате:\n'
-            '1. +375XXXXXXXXX\n'
-            '2. 80XXXXXXXXX'
+    message=error_message
 )
