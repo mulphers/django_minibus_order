842cb542a2f4462fd6c4f8cf6067b5a16fd80c7c
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-11-23 17:52:47 +0300

    Изменил импорты

--- users/views.py
+++ users/views.py
@@ -3,11 +3,13 @@ from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, UpdateView
 
-from common import mixins
-from users import forms, models
+from common import mixins as common_mixins
+from users import forms
+from users import mixins as users_mixins
+from users import models
 
 
-class SignInView(mixins.TitleMixin, mixins.LogoutRequiredMixin, LoginView):
+class SignInView(common_mixins.TitleMixin, users_mixins.LogoutRequiredMixin, LoginView):
     template_name = 'users/sign_in.html'
     title = _('Sign in')
     form_class = forms.SignInForm
@@ -16,7 +18,7 @@ class SignInView(mixins.TitleMixin, mixins.LogoutRequiredMixin, LoginView):
         return reverse_lazy('user:profile', kwargs={'pk': self.request.user.id})
 
 
-class SignUpView(mixins.TitleMixin, mixins.LogoutRequiredMixin, CreateView):
+class SignUpView(common_mixins.TitleMixin, users_mixins.LogoutRequiredMixin, CreateView):
     model = models.User
     form_class = forms.SignUpForm
     template_name = 'users/sign_up.html'
@@ -24,7 +26,7 @@ class SignUpView(mixins.TitleMixin, mixins.LogoutRequiredMixin, CreateView):
     success_url = reverse_lazy('user:sign_in')
 
 
-class ProfileView(mixins.TitleMixin, mixins.UserCheckMixin, UpdateView):
+class ProfileView(common_mixins.TitleMixin, users_mixins.UserCheckMixin, UpdateView):
     model = models.User
     form_class = forms.ProfileUpdateForm
     template_name = 'users/profile.html'
